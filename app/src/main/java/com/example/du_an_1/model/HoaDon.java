7c8fbfc921164c4a51dde81f2719fe79c8f9ecd3
anhph <https://github.com/Phucxyz/DuAn1_CarCare.git>
2022-11-17 23:18:29 +0700

    van phuc

---
+++ app/src/main/java/com/example/du_an_1/model/HoaDon.java
@@ -0,0 +1,16 @@
+package com.example.du_an_1.model;
+
+public class HoaDon {
+    public int MaHD;
+    public int MaKH;
+    public int MaDV;
+
+    public HoaDon() {
+    }
+
+    public HoaDon(int maHD, int maKH, int maDV) {
+        MaHD = maHD;
+        MaKH = maKH;
+        MaDV = maDV;
+    }
+}
