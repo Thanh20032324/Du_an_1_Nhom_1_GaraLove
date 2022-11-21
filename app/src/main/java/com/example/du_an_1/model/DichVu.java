eed876c5156dc317801d9a1723dd5411ce4d4caf
anhph <https://github.com/Phucxyz/DuAn1_CarCare.git>
2022-11-21 07:54:58 +0700

    phuc

--- app/src/main/java/com/example/du_an_1/model/DichVu.java
+++ app/src/main/java/com/example/du_an_1/model/DichVu.java
@@ -1,4 +1,14 @@
 package com.example.du_an_1.model;
 
 public class DichVu {
+    public int maDV;
+    public int tenDV;
+
+    public DichVu() {
+    }
+
+    public DichVu(int maDV, int tenDV) {
+        this.maDV = maDV;
+        this.tenDV = tenDV;
+    }
 }
