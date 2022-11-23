0ce7ef504e809e2c1a5632683ae55bf9e5187e31
thanh <thanhttph23851@fpt.edu.vn>
2022-11-23 09:06:37 +0700

    Fix loi

---
+++ app/src/main/java/com/example/du_an_1/Adapter/AdapterDichVu.java
@@ -0,0 +1,74 @@
+package com.example.du_an_1.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.ContentView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.du_an_1.R;
+import com.example.du_an_1.model.DichVu;
+import com.example.du_an_1.model.NhanVien;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class AdapterDichVu extends ArrayAdapter<DichVu> {
+    private Context context;
+    private int resource;
+    private List<DichVu> objects;
+    private LayoutInflater inflater;
+    private ArrayList<DichVu> arrayList;
+    public AdapterDichVu( Context context, int resource, List<DichVu> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.resource = resource;
+        this.objects = objects;
+        this.arrayList = new ArrayList<DichVu>();
+        this.arrayList.addAll(objects);
+        this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        ViewHolder holder = new ViewHolder();
+        if(convertView==null){
+            convertView = inflater.inflate(R.layout.item_lv_addnv,null);
+            holder.tvmanv = (TextView)convertView.findViewById(R.id.item_lv_username);
+            holder.tvtennv = (TextView)convertView.findViewById(R.id.item_lv_name);
+            holder.tvnamsinhnv = (TextView)convertView.findViewById(R.id.item_lv_pass);
+
+            holder.temp1 = (TextView)convertView.findViewById(R.id.temp_1);
+            holder.temp2 = (TextView)convertView.findViewById(R.id.temp_2);
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        DichVu dv = objects.get(position);
+        holder.tvmanv.setText(String.valueOf(dv.maDV));
+        holder.tvtennv.setText(dv.tenDV);
+
+        holder.temp1.setText("Mã Dịch Vụ");
+        holder.temp2.setText("Tên Dịch Vụ");
+        return convertView;
+    }
+//    public void filter(String charText){
+//        charText= charText.toLowerCase(Locale.getDefault());
+//        objects.clear();
+//        if(charText.length()==0){
+//            objects.addAll(arrayList);
+//        }else {
+//            for (DichVu dv : arrayList){
+//                if(dv.)
+//            }
+//        }
+//    }
+    public class ViewHolder{
+          TextView tvmanv,tvtennv,tvnamsinhnv,temp1,temp2;
+    }
+}
--- app/src/main/java/com/example/du_an_1/main/MainGiaoDien.java
+++ app/src/main/java/com/example/du_an_1/main/MainGiaoDien.java
@@ -1,16 +1,99 @@
 package com.example.du_an_1.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.du_an_1.Adapter.AdapterDichVu;
+import com.example.du_an_1.DAO.DichVuDAO;
+import com.example.du_an_1.DAO.NhanVienDAO;
 import com.example.du_an_1.R;
+import com.example.du_an_1.databinding.ActivityMainGiaoDienBinding;
+import com.example.du_an_1.model.DichVu;
+import com.example.du_an_1.model.NhanVien;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainGiaoDien extends AppCompatActivity {
+Context context = this;
+TextView nameuser;
+View view;
+NhanVien nhanVien;
+NhanVienDAO nhanVienDAO;
+List<NhanVien> nhanVienList;
 
+DichVuDAO dichVuDAO;
+AdapterDichVu adapterDichVu;
+List<DichVu> vuList;
+ListView listView;
+
+
+
+private AppBarConfiguration mAppBarConfiguration;
+private ActivityMainGiaoDienBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_giao_dien);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        vuList = new ArrayList<>();
+        dichVuDAO = new DichVuDAO(getApplicationContext());
+        vuList = dichVuDAO.getAll();
+        adapterDichVu = new AdapterDichVu(getApplicationContext(),R.layout.item_lv_addnv,vuList);
+        binding = ActivityMainGiaoDienBinding.inflate(
+                getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.appBarMaingiaodien.toolbar);
+
+        nhanVien = new NhanVien();
+        nhanVienDAO = new NhanVienDAO(getApplicationContext());
+        nhanVienList = new ArrayList<>();
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_qlphieuthu, R.id.nav_qlloaisach,R.id.nav_quanlysach,
+                R.id.nav_qlthanhvien,
+                R.id.nav_top,
+                R.id.nav_doanhthu,
+                R.id.nav_them,
+                R.id.nav_matkhau,
+                R.id.nav_dangxuat).setOpenableLayout(drawer).build();
+        NavController navController = Navigation.findNavController(this,
+                R.id.nav_host_fragment_content_maingiaodien);
+        NavigationUI.setupActionBarWithNavController(this,navController,mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView,navController);
+        view = navigationView.getHeaderView(0);
+        nameuser = view.findViewById(R.id.login_nameuser);
+        Intent intent = getIntent();
+        String user = intent.getStringExtra("user");
+        nhanVienList = nhanVienDAO.getAll();
+        if(user.equalsIgnoreCase("admin")){
+            navigationView.getMenu().findItem(R.id.nav_them).setVisible(true);
+        }
+        for (int i = 0; i<nhanVienList.size();i++){
+            if(nhanVienList.get(i).maNV.equals(user)){
+                nameuser.setText("Xin Chào"+nhanVienList.get(i).hoTen);
+                return;
+            }
+        }
+    }
+    @Override
+    public boolean onSupportNavigateUp(){
+        NavController navController = Navigation.findNavController(this,
+                R.id.nav_host_fragment_content_maingiaodien);
+        return NavigationUI.navigateUp(navController,mAppBarConfiguration) || super.onSupportNavigateUp();
     }
 }
